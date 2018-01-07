64419e1ab56a66ab83bac00b8e4cf6f0da0e6c19
Import-Python <Import-Python@users.noreply.github.com>
2018-01-06 22:49:21 -0500

    Fix issues with git pushing wrong version
    2.10

--- pineapple.js
+++ pineapple.js
@@ -1,7 +1,7 @@
 /**
  * PineApple - JavaScript Code Management System
  * @author Import-Python
- * @version 0.1.0
+ * @version 2.1.0
  */
 
 
@@ -202,6 +202,8 @@ class Stem {
         this.exportedStems = {}
     }
     add(directory, id, settings) {
+        console.log(id)
+        console.log(name)
         if (this.allStems[id] == null) {
             let data = {}
             data.id = id
@@ -213,6 +215,8 @@ class Stem {
         }
     }
     export(id, name, instance) {
+        console.log(id)
+        console.log(name)
         this.exportedStems[id][name] = new instance()
     }
     getStem(id, name) {
@@ -268,9 +272,9 @@ class Complier {
                     code = this._encode(id, code, chunk, settings)
                 }
             } else {
-                //Haha u don't has permission!
             }
         }
+        code = replaceAll(code, `import "`, `var _ = "`)
         return code
 
     }
@@ -469,6 +473,7 @@ class Handler {
 
         ///////////////
         let vmCode = `module.exports = function(_export, ${uuidChunkList}${uuidStemList}) { ${code} }`
+        console.log(vmCode)
         let run = this.vm[directory].run(vmCode);
         let doRun = `run(_export, ${runtimeChunkList}${runtimeStemList})`
         eval(doRun)
@@ -490,7 +495,7 @@ class Core {
 
             let id = splitAt(f.lastIndexOf("/"))(f)[0]
             id = replaceAll(id, "/", ".").toUpperCase()
-            PineApple.Handler.loadChunk(id, `./${f}`, settings)
+            PineApple.Handler.loadChunk(id, `${f}`, settings)
         }
     }
     enableChunks() {
@@ -498,24 +503,48 @@ class Core {
         PineApple.Chunks.cut()
     }
     useStem(directory, settings) {
+        console.log(directory)
         PineApple.Handler.createVM(directory, settings)
         let files = walkSync(directory)
+        let folder = ""
+        if (settings.base != null) {
+            console.log("FOLDFER")
+            folder = directory.replace(/\\/g, "/");
+            folder = splitAt(folder.lastIndexOf("/"))(folder)[0]
+            folder = splitAt(folder.lastIndexOf("/"))(folder)[1]
+            folder = replaceAll(folder, "/", "")
+        }
         for (let file in files) {
             this.totalStems++
             let f = files[file]
-            f = f.replace(/\\/g, "/");
-            let id = splitAt(f.lastIndexOf("/"))(f)[0]
-            id = replaceAll(id, "/", ".").toUpperCase()
+            let id = ""
+            if (settings.base != null) {
+                f = f.replace(/\\/g, "/");
+                id = splitAt(f.lastIndexOf(folder))(f)[1]
+                id = splitAt(id.lastIndexOf("/"))(id)[0]
+                id = replaceAll(id, "/", ".").toUpperCase()
+            } else {
+                f = f.replace(/\\/g, "/");
+                id = splitAt(f.lastIndexOf("/"))(f)[0]
+                id = replaceAll(id, "/", ".").toUpperCase()
+            }
             PineApple.Stem.add(directory, id, settings)
         }
         for (let file in files) {
             this.totalStems++
             let f = files[file]
-            f = f.replace(/\\/g, "/");
-
-            let id = splitAt(f.lastIndexOf("/"))(f)[0]
-            id = replaceAll(id, "/", ".").toUpperCase()
-            PineApple.Handler.loadStem(directory, id, `./${f}`, settings)
+            let id = ""
+            if (settings.base != null) {
+                f = f.replace(/\\/g, "/");
+                id = splitAt(f.lastIndexOf(folder))(f)[1]
+                id = splitAt(id.lastIndexOf("/"))(id)[0]
+                id = replaceAll(id, "/", ".").toUpperCase()
+            } else {
+                f = f.replace(/\\/g, "/");
+                id = splitAt(f.lastIndexOf("/"))(f)[0]
+                id = replaceAll(id, "/", ".").toUpperCase()
+            }
+            PineApple.Handler.loadStem(directory, id, `${f}`, settings)
         }
     }
 }
